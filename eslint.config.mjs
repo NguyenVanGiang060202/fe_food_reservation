4521458c282f8f314f05c6d0e8bea62026f6fc91
NguyenVanGiang060202 <nguyenvangiang060202@gmail.com>
2025-06-27 15:32:08 +0700

    Update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,19 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn", // hoặc "off" nếu bạn muốn tắt hẳn
+        {
+          varsIgnorePattern: "^_",
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
 ];
+
+
 
 export default eslintConfig;
